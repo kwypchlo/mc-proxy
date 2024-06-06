d761bbfda86ab96c6cb0a468149573d54f331419
Karol Wypchlo <kwypchlo@gmail.com>
2024-06-06 14:18:17 +0200

    initial proxy code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,80 @@
 import { Hono } from "hono";
+import ky, { HTTPError } from "ky";
+import TTLCache from "@isaacs/ttlcache";
+import config from "../.config.json";
+
+function rand(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+if (config.apiTokens.length < 3) {
+  throw new Error("Provide all the required API tokens in the .env.json file");
+}
+
+const apiTokens = config.apiTokens;
+const ttl = 60_000; // 60 seconds max age
+const endpoint = "https://api.twitter.com/2/tweets/search/all";
+
+let cacheMiss = 0;
+let cacheHits = 0;
+
+const cache = new TTLCache<string, Promise<any>>({ ttl });
+
+const tweets = async (search: string): Promise<any> => {
+  if (cache.has(search)) {
+    cacheHits++;
+    return cache.get(search);
+  }
+
+  const promise = new Promise(async (resolve) => {
+    try {
+      const data = await ky(endpoint, {
+        searchParams: search,
+        headers: {
+          authorization: `Bearer ${apiTokens[cacheMiss++ % apiTokens.length]}`,
+          "user-agent": "v2FullArchiveSearchPython",
+        },
+        retry: { limit: 20, delay: () => rand(1000, 2000) },
+      }).json();
+
+      cache.setTTL(search, ttl);
+
+      return resolve({ data, status: 200 });
+    } catch (error) {
+      cache.delete(search);
+
+      if (error instanceof HTTPError) {
+        console.log("\t", "[Twitter Api HTTPError]", error.response.status, error.response.statusText);
+
+        return resolve({ data: undefined, status: error.response.status });
+      }
+
+      console.log("\t", "[Error]", String(error));
+
+      return resolve({ data: undefined, status: 500 });
+    }
+  });
+
+  cache.set(search, promise);
+
+  return promise;
+};
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.get("/", async (c) => {
+  const { search } = new URL(c.req.url);
+  const { data, status } = await tweets(search);
+
+  console.log(`GET [status: ${status}] ${search}`);
+
+  if ((cacheHits + cacheMiss) % 1 === 0) {
+    const ratio = Math.floor((cacheHits / (cacheHits + cacheMiss)) * 100) || 0;
+
+    console.log(`📦 Cache: misses ${cacheMiss}, hits ${cacheHits} (${ratio}%), size ${cache.size}`);
+  }
+
+  return c.json(data, status);
 });
 
 export default app;
