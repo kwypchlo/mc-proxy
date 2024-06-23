a37206abeb682804a8a476609253487edcab4583
Karol Wypchlo <kwypchlo@gmail.com>
2024-06-23 15:23:32 +0200

    retain ratio

--- src/api/stats.ts
+++ src/api/stats.ts
@@ -3,9 +3,13 @@ import { cache, cacheStats } from "../cache";
 import { config } from "../config";
 
 export const statsApi = async (c: Context) => {
+  const cacheRatio = Math.floor((cacheStats.hits / (cacheStats.hits + cacheStats.misses)) * 100) || 0;
+  const retainRatio = Math.floor((cacheStats.retained / (cacheStats.fetched + cacheStats.retained)) * 100) || 0;
+
   return c.json({
     ...cacheStats,
-    ratio: `${Math.floor((cacheStats.hits / (cacheStats.hits + cacheStats.misses)) * 100) || 0}%`,
+    ratio: `${cacheRatio}%`,
+    retainRatio: `${retainRatio}%`,
     size: cache.size,
     ttl: config.cache.ttl,
   });
