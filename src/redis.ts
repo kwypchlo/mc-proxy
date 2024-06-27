447ac240e92c21bf151a3dc79710da63558dab35
Karol Wypchlo <kwypchlo@gmail.com>
2024-06-27 17:22:41 +0200

    reuse redis instance

--- src/redis.ts
+++ src/redis.ts
@@ -1,4 +1,15 @@
 import Redis from "ioredis";
 import { config } from "./config";
 
-export const redisClient = new Redis(config.redis.url);
+declare global {
+  var redisClient: Redis;
+}
+
+const createClient = async (url: string) => {
+  if ("redisClient" in globalThis) {
+    return globalThis.redisClient;
+  }
+  return (globalThis.redisClient = new Redis(url, { enableAutoPipelining: true }));
+};
+
+export const redisClient = await createClient(config.redis.url);
